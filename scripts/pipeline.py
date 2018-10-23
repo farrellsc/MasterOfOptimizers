6ae73578eea00b3eb06a6f767d363980a214c442
farrellsc <farrellsc@163.com>
2018-10-22 22:46:31 -0400

    added optimizers

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,16 +1,14 @@
 from MasterOfOptimizers.classifier.LogisticRegression import LogisticRegression
 from MasterOfOptimizers.optimizer.MiniBatchGD import MiniBatchGD
 from MasterOfOptimizers.optimizer.AdaGrad import AdaGrad
+from MasterOfOptimizers.optimizer.AdaDelta import AdaDelta
+from MasterOfOptimizers.optimizer.RMSProp import RMSProp
+from MasterOfOptimizers.optimizer.ADAM import ADAM
 from MasterOfOptimizers.dataloader.BaseDataloader import BaseDataloader
 import matplotlib.pyplot as plt
 
 
 def main():
-    trainDataloader = BaseDataloader(
-        file_path="../data/fake1",
-        batch_size=5
-    )
-
     trainDataloader = BaseDataloader(
         file_path="/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/data/fake1",
         batch_size=100
@@ -35,10 +33,40 @@ def main():
         trainDataloader,
         "/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/plots/adagrad_testcase_boundary.png")
 
+    adadelta_model = LogisticRegression(
+        optimizer=AdaDelta(),
+        num_iter=iters
+    )
+    adadelta_model.train(trainDataloader)
+    adadelta_model.plot(
+        trainDataloader,
+        "/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/plots/adadelta_testcase_boundary.png")
+
+    rmsprop_model = LogisticRegression(
+        optimizer=RMSProp(),
+        num_iter=iters
+    )
+    rmsprop_model.train(trainDataloader)
+    rmsprop_model.plot(
+        trainDataloader,
+        "/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/plots/rmsprop_testcase_boundary.png")
+
+    adam_model = LogisticRegression(
+        optimizer=ADAM(),
+        num_iter=iters
+    )
+    adam_model.train(trainDataloader)
+    adam_model.plot(
+        trainDataloader,
+        "/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/plots/adam_testcase_boundary.png")
+
     xs = [i+1 for i in range(iters)]
     plt.plot(xs, minibatch_model.get_loss_history())
     plt.plot(xs, adagrad_model.get_loss_history())
-    plt.legend(["miniBatch", "adagrad"])
+    plt.plot(xs, adadelta_model.get_loss_history())
+    plt.plot(xs, rmsprop_model.get_loss_history())
+    plt.plot(xs, adam_model.get_loss_history())
+    plt.legend(["miniBatch", "adagrad", "adadelta", "rmsprop", "adam"])
     plt.savefig("/media/zzhuang/00091EA2000FB1D0/iGit/git_projects/MasterOfOptimizers/plots/loss/multiple_loss.png")
 
 
